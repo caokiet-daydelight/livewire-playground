698f5bb053814b73906118cacb490206f29c82c4
カオ・キェット <kiet@daydelight.com>
2023-07-03 14:42:12 +0900

    add tailwindcss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
 
     <!-- Styles -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
     <style>
         /* ! tailwindcss v3.2.4 | MIT License | https://tailwindcss.com */
